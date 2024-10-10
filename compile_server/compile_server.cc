f1c265a8b82d35a03caaea676886302c4c73cefb
shawyxy <9inghuan@gmail.com>
2024-10-10 16:48:00 -0400

    POST测试

--- compile_server/compile_server.cc
+++ compile_server/compile_server.cc
@@ -8,12 +8,23 @@ using namespace httplib;
 
 int main()
 {
-    // 1. 创建server对象
+    // 1. 创建Server对象
     Server svr;
-    // 2. 注册响应内容
-    //    当用户请求"/hello"时，执行对应的响应（lambda函数）
-    svr.Get("/hello", [](const Request &req, Response &resp)
-            { resp.set_content("hello httplib, 你好", "text/plain;charset=utf-8;"); });
+
+    // 2. 注册响应处理
+    //    当用户请求"/compile_and_run"时，执行对应的响应（编译并运行）
+    svr.Post("/compile_and_run", [](const Request &req, Response &resp) {
+        // 获取用户提交的代码
+        std::string in_json = req.body; 
+        // 输出型参数：代码编译运行后的结果（状态码，描述，[标准输出，标准错误]）
+        std::string out_json; 
+        if (!in_json.empty()) { // 请求体不为空
+            // 调用编译并运行代码的函数
+            CompileAndRun::Start(in_json, &out_json);
+            // 返回编译运行结果
+            resp.set_content(out_json, "application/json;charset=utf-8");
+        }});
+
     // 3. 启动http服务
     svr.listen("0.0.0.0", 8080);
     return 0;
